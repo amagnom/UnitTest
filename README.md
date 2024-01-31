efaf99a15b2f4043ea6f167b9eb62ca59f5b6aaa
Arildo <arildodraix@gmail.com>
2024-01-31 07:41:17 -0300

    Project and gitignore and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Unit Test
+A simple project using the concept of unit test.
+
+## What is
+Unit testing is the entire application of testing on the input and output signatures of a system. It consists of validating valid and invalid data via I/O being applied by developers or test analysts. A unit is the smallest testable part of a computer program.
+
+### Pics
+Result of test\
+![ResultTest](https://github.com/amagnom/)
+
+
---
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,25 @@
+﻿using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+   
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(5)]
+        public void IsPrime_ReturnTrue(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} is prime");
+        }
+
+    }
+}
